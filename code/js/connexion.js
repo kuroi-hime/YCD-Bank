bfe78660c2a7d768d40db654a5a8546273a46882
kuroi-hime <kuroihime095@gmail.com>
2025-11-12 07:16:59 +0000

    fix: stay authentified problem

--- code/js/connexion.js
+++ code/js/connexion.js
@@ -1,15 +1,9 @@
-const nameRegex = /^[a-zA-Z\s-']{2,50}$/;
-const cinRegex = /^[A-Z]{1,2}\d{6,8}$/;
-const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
-const phoneRegex = /^(05|06|07)\d{8}$/;
-const passwordRegex = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{5,}$/;
-
 document.querySelector('button').addEventListener('click', ()=>{
     let identifiant = document.getElementById('identifiant');
     let password = document.getElementById('password');
     let bankData = JSON.parse(localStorage.getItem('YCD_Bank'));
 
-    let currentUser = bankData.find(user => user.comptes.find(compte => compte.type == 'Principale' && compte.rib.includes(identifiant.value)));
+    let currentUser = bankData.find(user => user.comptes.find(compte => compte.type == 'Principal' && compte.rib.includes(identifiant.value)));
     let a = document.createElement('a');
     if(currentUser){
         if(currentUser.password == password.value){
@@ -19,6 +13,7 @@ document.querySelector('button').addEventListener('click', ()=>{
             // xhr.send(JSON.stringify(currentUser));
             // xhr.onload = 
             localStorage.setItem('currentUser', JSON.stringify({index: bankData.indexOf(currentUser)}))
+            console.log(localStorage.getItem(currentUser));
             // a changer
             a.href = '../compte.html'
             a.click();
