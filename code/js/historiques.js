9d5fcb362941090ce70c94562024bbeef101f7e9
kuroi-hime <kuroihime095@gmail.com>
2025-11-14 23:10:04 +0100

    feat:historiques

--- code/js/historiques.js
+++ code/js/historiques.js
@@ -1,4 +1,15 @@
 let historiques = [];
+let menuIcon = document.getElementById("menu-icon");
+let navbar = document.getElementById("menu");
+let btnDeconnecter = document.getElementById("deconnecter");
+
+btnDeconnecter.addEventListener('click', ()=>{if(confirm("Voulez-vous vraiment vous déconnecter?")){
+    let a = document.createElement('a');
+    a.href = 'connexion.html';
+    a.click();
+    localStorage.removeItem('currentUser');
+}});
+menuIcon.addEventListener('click', ()=>{navbar.classList.toggle('hidden')});
 
 function historiqueTemplate(h){
     return `
