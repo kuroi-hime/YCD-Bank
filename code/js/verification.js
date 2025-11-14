a7a9553a82f866d68d0e7289de92c581b89c7ab3
kuroi-hime <kuroihime095@gmail.com>
2025-11-14 07:21:11 +0000

    feat:verification

--- code/js/verification.js
+++ code/js/verification.js
@@ -1,3 +1,11 @@
+let code = JSON.parse(localStorage.getItem('code'));
+let input1 = document.getElementById('chiffre1');
+let input2 = document.getElementById('chiffre2');
+let input3 = document.getElementById('chiffre3');
+let input4 = document.getElementById('chiffre4');
+let input5 = document.getElementById('chiffre5');
+let alertFaild = document.getElementById('alertFaild');
+
 function generateCode(){
     let chiffre1 = Math.floor(Math.random() * 10);
     let chiffre2 = Math.floor(Math.random() * 10);
@@ -32,17 +40,25 @@ function genererRIB(){
 
 document.querySelector('button').addEventListener('click',()=>{
     let data = JSON.parse(sessionStorage.getItem('inscriptionData'));
-    
-    let bankData = JSON.parse(localStorage.getItem('YCD_Bank'))||[];
-    data.comptes = [{rib: genererRIB(), type: "Principal", credit: 10000}, {rib: genererRIB(), type: "Epargne", credit: 0}];
-    data.factures = [];
-    data.recharges = [];
-    data.virements = [];
+    let codeEntre = input1.value+input2.value+input3.value+input4.value+input5.value;
+    if(code == codeEntre){
+      let bankData = JSON.parse(localStorage.getItem('YCD_Bank'))||[];
+      data.comptes = [{rib: genererRIB(), type: "Principal", credit: 10000}, {rib: genererRIB(), type: "Epargne", credit: 0}];
+      data.factures = [];
+      data.recharges = [];
+      data.virements = [];
 
-    bankData.push(data);
-    localStorage.setItem('YCD_Bank', JSON.stringify(bankData));
-    sessionStorage.clear();
-    let a = document.createElement('a');
-    a.href = "../html/connexion.html";
-    a.click();
+      bankData.push(data);
+      localStorage.setItem('YCD_Bank', JSON.stringify(bankData));
+      sessionStorage.clear();
+      let a = document.createElement('a');
+      a.href = "../html/connexion.html";
+      a.click();
+    }
+    else{
+      alertFaild.innerText = 'Le code que vous avez entre ne correspend pas au celui envoye.';
+      alertFaild.classList.remove('hidden');
+      setTimeout();
+    }
+    
 });
