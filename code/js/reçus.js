266744e55ffad84603a8cb75c2837a91d8910d80
kuroi-hime <kuroihime095@gmail.com>
2025-11-14 23:14:18 +0100

    feat:reçus

--- code/js/reçus.js
+++ code/js/reçus.js
@@ -1,4 +1,14 @@
+let menuIcon = document.getElementById("menu-icon");
+let navbar = document.getElementById("menu");
+let btnDeconnecter = document.getElementById("deconnecter");
 
+btnDeconnecter.addEventListener('click', ()=>{if(confirm("Voulez-vous vraiment vous déconnecter?")){
+    let a = document.createElement('a');
+    a.href = 'connexion.html';
+    a.click();
+    localStorage.removeItem('currentUser');
+}});
+menuIcon.addEventListener('click', ()=>{navbar.classList.toggle('hidden')});
 
 let bankData = JSON.parse(localStorage.getItem('YCD_Bank'));
 let currentUser = JSON.parse(localStorage.getItem('currentUser'));
